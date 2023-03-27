a0553657103f40b65b0cdadef71eeaaa4817b021
Merthan Karakaş <119036743+thehellan@users.noreply.github.com>
2023-03-27 14:46:11 +0300

    Update Centrafuge.ino

--- Centrafuge.ino
+++ Centrafuge.ino
@@ -1,9 +1,11 @@
 #include <LiquidCrystal_I2C.h>
 #include <ESC.h>
 
+// LCD ekran ve ESC nesneleri oluşturuluyor
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 ESC myESC;
 
+// Değişkenler tanımlanıyor
 int motorDevri = 0;
 int dakika = 0;
 int dakikaKalan = 0;
@@ -13,6 +15,7 @@ int azaltmaButton = 8;
 int buzzer = 9;
 
 void setup() {
+  // LCD ekranı başlatılıyor ve sistem hazırlanıyor mesajı gösteriliyor
   lcd.init();
   lcd.backlight();
   lcd.print("Hoş Geldiniz!");
@@ -21,6 +24,8 @@ void setup() {
   lcd.print("Sistem kontrol ediliyor");
   delay(5000);
   lcd.clear();
+
+  // Butonlar ve ESC için gerekli pinler tanımlanıyor
   pinMode(startButton, INPUT_PULLUP);
   pinMode(arttirmaButton, INPUT_PULLUP);
   pinMode(azaltmaButton, INPUT_PULLUP);
@@ -29,6 +34,7 @@ void setup() {
 }
 
 void loop() {
+  // Motor devri seçimi yapılıyor
   lcd.setCursor(0, 0);
   lcd.print("Motor Devri:");
   lcd.setCursor(0, 1);
@@ -53,6 +59,8 @@ void loop() {
       delay(500);
     }
   }
+
+  // Dakika seçimi yapılıyor
   lcd.clear();
   lcd.print("Dakika:");
   lcd.setCursor(0, 1);
@@ -77,10 +85,13 @@ void loop() {
       delay(500);
     }
   }
+
+  // Başlatma butonu bekleniyor
   lcd.clear();
   lcd.print("Onayla: *Baslat");
   while (digitalRead(startButton) == HIGH) {
     if (digitalRead(arttirmaButton) == LOW) {
+      // Motor hızı ayarlanıyor ve geri sayım başlatılıyor
       int pwm = map(motorDevri, 0, 10000, 1000, 2000);
       myESC.writeMicroseconds(pwm); // Opto-ESC'yi belirtilen hızda çalıştır
       dakikaKalan = dakika;
@@ -92,17 +103,21 @@ void loop() {
         delay(60000);
         dakikaKalan--;
       }
-      for (int i = 2000; i >= 1000; i -= 10) {
-        myESC.writeMicroseconds(i); // Opto-ESC'yi yavaşlatarak durdur
-        delay(10);
-      }
-      digitalWrite(buzzer, HIGH);
-      delay(1000);
-      digitalWrite(buzzer, LOW);
-      delay(500);
-    }
-  }
-  motorDevri = 0;
-  dakika = 0;
-  lcd.clear();
+
+      // Motor yavaşlatılıyor ve sesli uyarı veriliyor
+     for (int i = 2000; i >= 1000; i -= 10) {
+    myESC.writeMicroseconds(i); // Opto-ESC'yi yavaşlatarak durdur
+  delay(10);
+}
+digitalWrite(buzzer, HIGH);
+delay(1000);
+digitalWrite(buzzer, LOW);
+delay(500);
+}
+}
+
+// Değişkenler sıfırlanıyor ve LCD ekran temizleniyor
+motorDevri = 0;
+dakika = 0;
+lcd.clear();
 }
